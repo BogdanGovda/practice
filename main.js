ea80b4c92391f2f1b46267f4a5e714efb87119e8
Bogdan <autoquest04@gmail.com>
2023-07-07 22:42:57 +0300

    test 1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict";
+let numberOfFilms = prompt("How more films do you sees? ");
+let personalMovie = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	generes:[],
+	privat: false
+};
+let nameMovies = prompt("your last movie: ");
+let gradeMovie = prompt("how many star do you give? ");
+personalMovie.movies[nameMovies] = gradeMovie;
+console.log(personalMovie);
